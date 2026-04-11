21cefa0bc94727ee7519ba155b781b0e9b236665
Fauzst <business.joshua.pura@gmail.com>
2026-04-11 23:10:15 +0800

    Fix: Added MongoDB connection

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,24 +1,29 @@
-import { MongoClient } from 'mongodb';
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = process.env.MONGODB_URI;
-const options = {};
+require('dotenv').config({
+  path: '.env.local'
+});
 
-let client;
-let clientPromise;
+const uri = process.env.MONGO_URI;
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your Mongo URI to .env.local');
-}
-
-if (process.env.NODE_ENV === 'development') {
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
-  clientPromise = global._mongoClientPromise;
-} else {
-  client = new MongoClient(uri, options);
-  clientPromise = client.connect();
-}
+});
 
-export default clientPromise;
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
